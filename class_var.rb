dca8b6d673473c35a167fbacb5fe53077096078f
Fan-Chen Pai <fanchen.pai@gmail.com>
2013-10-09 13:28:45 -0700

    added constant

--- class_var.rb
+++ class_var.rb
@@ -4,14 +4,22 @@
 
 class Parent
   @@class_var = 'parent'
+
   def self.print_class_var
     puts @@class_var
   end
+
+  private
+  PARENT_CONSTANT = 'parent constant'
 end
 
 class Child < Parent
   @@class_var = 'child'
+  CHILD_CONST = 'child constant'
 end
 
 Parent.print_class_var # => will print "child"
 Child.print_class_var
+
+puts "#{Child::CHILD_CONST}"
+puts "#{Child::PARENT_CONSTANT}"
