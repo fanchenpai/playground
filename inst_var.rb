6728f583d152b815e205204841b1756c6d74d5f2
Fan-Chen Pai <fanchen.pai@gmail.com>
2013-10-09 12:48:18 -0700

    added inst_var.rb

---
+++ inst_var.rb
@@ -0,0 +1,15 @@
+class Parent
+  @instance_var = 'parent'
+  def print_var
+    puts @instance_var
+  end
+end
+
+class Child < Parent
+  def initialize
+    @instance_var = 'child'
+  end
+end
+
+c.print_var
+p.print_var
